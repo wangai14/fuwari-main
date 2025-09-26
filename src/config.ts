5f831cb0d76123b315cf034ccf85a174ceba2373
ATM意中人 <79092517+wangai14@users.noreply.github.com>
2025-09-26 08:47:49 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "码境Code Realm",
-	subtitle: "爱你一生",
+	subtitle: "爱你一生世",
 	lang: "zh_CN", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
